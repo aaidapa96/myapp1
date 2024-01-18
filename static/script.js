d45be65a7c794293649b739a5c4570173493cb27
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2024-01-18 21:02:06 +0530

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,29 @@
+
+// script.js
+function submitForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+  
+    // Send data to the backend (e.g., using AJAX)
+    // Use the server URL instead of 'file:///C:/submit'
+    fetch('https://20.219.54.63:3000/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: name, email: email }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Display the result message on the web page
+        document.getElementById("resultMessage").textContent = data.message;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+  }
+  
+  
+  
+  
+  
