5bb36048f3262d1dec3961c1930fbcd0273be5f9
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2024-01-23 17:06:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
-git clone https://github.com/aaidapa96/myapp1
-cd myapp1
-bash script.sh
+sudo apt update
+sudo apt install python3
+sudo apt install python3 python3-pip -y
+pip3 install Flask
+pip3 install Flask-CORS
+python3 app.py
