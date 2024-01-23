8a792004c6e189857177fca523dbcee9377073e5
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2024-01-23 15:53:00 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,3 @@
-sudo apt update
-sudo apt install python3
-sudo apt install python3 python3-pip -y
-pip3 install Flask
-pip3 install Flask-CORS
-# Install Python dependencies (if you have a requirements.txt file)
-#pip3 install -r requirements.txt
-sudo apt-get install openssl
 git clone https://github.com/aaidapa96/myapp1
 cd myapp1
-python3 app_http.py
+bash script.sh
